8a064f75f79afd929a745b27a577b59f12efdbd0
tsioumioupiou <g-anastasiou@hotmail.com>
2025-09-19 12:18:07 +0200

    First

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Navigation from '@/components/Navigation';
+
+
+export default function Home() {
+  return (
+    <>
+    <div>
+     <Navigation/>
+    </div>
+    </>
+      );
+}
---
+++ components/Navigation.tsx
@@ -0,0 +1,32 @@
+import { navItems } from "@/lib/constants";
+import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
+export default function Navigation() {
+  return (
+    <nav className="sticky top-0 z-50 bg-background/0 backdrop-blur-md border-b border-border/50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          {/* Logo */}
+          <div className="flex-shring-0">
+            <h1 className="text-xl font serif font-bold text-foreground">
+              Resonance
+            </h1>
+          </div>
+          {/* Desktop Navigation */}
+          <div className="hidden md:flex items-center space-x-8">
+            {navItems.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-sm font-medium text-muted-foreground hover:text-foreground transition-colors duration-200 "
+              >
+                {item.name}
+              </Link>
+            ))}
+            <ThemeToggle/>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
